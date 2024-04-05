e073019cc92bde9d13a6c928a3d7445eb64b1b4b
alemagno10 <alemagno03@gmail.com>
2024-04-05 17:40:01 -0300

    fix: controller interface

--- src/main/java/insper/store/partner/PartnerController.java
+++ src/main/java/insper/store/partner/PartnerController.java
@@ -2,8 +2,12 @@ package insper.store.partner;
 
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 
 @FeignClient(name = "store-partner")
 public interface PartnerController {
@@ -14,13 +18,13 @@ public interface PartnerController {
     );
 
     @GetMapping("/partner")
-    ResponseEntity<LoginOut> read (
-        @RequestHeader(required = true, name = "id-partner") String idUser
+    ResponseEntity<PartnerOut> read (
+        @RequestHeader(required = true, name = "id-partner") String idPartner
     );
 
     @PutMapping("/partner/{id}")
-    public ResponseEntity<AccountOut> update (
+    public ResponseEntity<PartnerOut> update (
         @PathVariable(required = true) String id,
-        @RequestBody(required = true) AccountIn in
+        @RequestBody(required = true) PartnerIn in
     );
 }
